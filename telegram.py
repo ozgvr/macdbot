4eceeacad4cc16167057a8b8a4806b1598cc9e0f
Özgür Akın <ozgurakn14@gmail.com>
2022-08-01 10:44:55 +0300

    fix winrate divide by zero

--- telegram.py
+++ telegram.py
@@ -3,7 +3,7 @@ import config
 
 def send_alert(trades):
     total_profit = str(round(((trades["profit"]-1)*100),2))+"%"
-    winrate = str(round((trades["winning_trades"]/trades["closed_trades"])*100,2))+"%"
+    winrate = str(round((trades["winning_trades"]/trades["closed_trades"])*100,2))+"%" if trades["closed_trades"]>0 else "0%"
     symbol = trades["trades"][0]["ticker"][:-4]
     buy_price = trades["trades"][0]["position_buy_price"]
     if("position_sell_price" not in trades["trades"][0]):
