c5244cc87c194169fa6ad0671e0d1de3601f2859
Özgür Akın <ozgurakn14@gmail.com>
2022-08-06 03:28:36 +0300

    change json file key order

--- reset.py
+++ reset.py
@@ -4,11 +4,11 @@ def reset(account):
     trades_file = open("data.json","w")
     data = {}
     data["account"]=account
-    data["trades"]=[]
+    data["profit"]=1
     data["winning_trades"]=0
     data["open_trades"]=0
     data["closed_trades"]=0
-    data["profit"]=1
+    data["trades"]=[]
     json.dump(data,trades_file, indent=2)
     trades_file.close()
 
