4a614b130405d719e74e0e4247ecacc5d0a89aa8
Özgür Akın <ozgurakn14@gmail.com>
2022-08-06 03:27:33 +0300

    fix file json loads

--- reset.py
+++ reset.py
@@ -2,15 +2,14 @@ import json
 
 def reset(account):
     trades_file = open("data.json","w")
-    data = json.loads(trades_file.read())
+    data = {}
     data["account"]=account
     data["trades"]=[]
     data["winning_trades"]=0
     data["open_trades"]=0
     data["closed_trades"]=0
     data["profit"]=1
-    trades_file.seek(0)
-    json.dump(data,trades_file)
+    json.dump(data,trades_file, indent=2)
     trades_file.close()
 
 if __name__ == "__main__":
